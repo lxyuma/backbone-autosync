c664a51cbba775cc8a417faac376b6837bed2455
ryuma.tsukano <lyumalyuma@gmail.com>
2013-12-02 15:03:06 +0900

    mod beforeEach

--- test/autosync.js
+++ test/autosync.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
 
   module('Backbone.Autosync');
 
-asyncTest('set', function() {
   var MyModel = Backbone.Model.extend({urlRoot: "faked"});
   this.myInstance = new MyModel({title: "first"});
 
-  this.myInstance.autoSave();
-  this.myInstance.set({title: "changed"});
+  asyncTest('save', _.bind(function() {
 
-  this.spySave = sinon.spy(this.myInstance, "save");
+    this.myInstance.autoSave();
+    this.myInstance.set({title: "changed"});
+    this.spySave = sinon.spy(this.myInstance, "save");
 
-  setTimeout(_.bind(function() {
-    deepEqual(this.myInstance.nowSyncLock, false);
-    ok(this.spySave.calledOnce);
-    start();
-  }, this), 2000);
-});
+    setTimeout(_.bind(function() {
+      deepEqual(this.myInstance.nowSyncLock, false);
+      ok(this.spySave.calledOnce);
+      start();
+    }, this), 2000);
+  }, this));
 
 
 });
